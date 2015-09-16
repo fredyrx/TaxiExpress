77e3c2839d4708838ec9b2c68622f70f8592126f
fredyrx <fredy.rx@gmail.com>
2015-09-16 20:57:10 +0000

    add icons to layouts

--- taxiexpress/vendor/assets/javascripts/application.js
+++ taxiexpress/vendor/assets/javascripts/application.js
@@ -2,3 +2,5 @@
 //= require binary-admin/morris/raphael-2.1.0.min
 //= require binary-admin/morris/morris
 //= require binary-admin/custom
+//= require binary-admin/dataTables/dataTables.bootstrap
+//= require bonary-admin/dataTables/jquery.dataTables
